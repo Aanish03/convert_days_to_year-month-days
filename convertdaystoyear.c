afb8ad667e4b1c4359808f48e6503bb8e539cb58
Aanish rajendra Deshmukh <83102459+anish936@users.noreply.github.com>
2022-04-13 08:59:24 +0530

    Add files via upload

---
+++ convertdaystoyear.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int days;
+    printf("enter the number of days ");
+    scanf("%d",&days);
+
+    int years;
+    int months;
+
+    if(days%365 == 0  ){
+        years = days/365 ;
+
+    }
+    else {
+        years = days/365;
+
+        days = days-(years*365);
+
+        months = days/30; 
+
+        days = days-(months*30);
+
+
+
+
+
+
+        
+    }
+
+    printf("%d years %d months %d days " , years , months , days );
+    
+    getch();
+    
+    
+}
